3040dfb6ef66e4f2cd02e589cacbfeed8589d4c1
Dennis Pitallano <dennis.pitallano@hotmail.co.uk>
2021-02-13 23:56:49 +0800

    update master class forum app

--- chapter1/pita-app1/.eslintrc.js
+++ chapter1/pita-app1/.eslintrc.js
@@ -5,18 +5,18 @@ module.exports = {
     node: true
   },
 
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/standard'
+  ],
+
   parserOptions: {
     parser: 'babel-eslint'
   },
 
   rules: {
-    'no-unused-vars': 'warn',
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  },
-  
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard'
-  ]
+  }
 }
